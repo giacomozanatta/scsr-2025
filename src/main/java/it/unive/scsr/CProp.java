72960fb0f3bffba29a22c14cd6132d790c08c53b
SimoneBiondo <879899@stud.unive.it>
2025-03-02 10:29:02 +0100

    CP-Task1: Implement constant propagation

--- src/main/java/it/unive/scsr/CProp.java
+++ src/main/java/it/unive/scsr/CProp.java
@@ -1,49 +1,125 @@
 package it.unive.scsr;
 
-public class CProp {
+import it.unive.lisa.analysis.ScopeToken;
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.dataflow.DataflowElement;
+import it.unive.lisa.analysis.dataflow.DefiniteDataflowDomain;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.Constant;
+import it.unive.lisa.symbolic.value.Identifier;
+import it.unive.lisa.symbolic.value.ValueExpression;
+import it.unive.lisa.util.representation.ListRepresentation;
+import it.unive.lisa.util.representation.StringRepresentation;
+import it.unive.lisa.util.representation.StructuredRepresentation;
+import it.unive.scsr.cp.ConstantPropagationData;
+import it.unive.scsr.cp.ConstantPropagationEvaluator;
+import it.unive.scsr.cp.EmptyHandler;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
-    // IMPLEMENTATION NOTE:
-    // the code below is outside of the scope of the course. You can uncomment
-    // it to get your code to compile. Be aware that the code is written
-    // expecting that a field named "id" and a field named "constant" exist
-    // in this class: if you name them differently, change also the code below
-    // to make it work by just using the name of your choice instead of
-    // "id"/"constant". If you don't have these fields in your
-    // solution, then you should make sure that what you are doing is correct :)
+public class CProp implements DataflowElement<DefiniteDataflowDomain<CProp>, CProp> {
 
-    // - Implement your solution using the DefiniteDataFlowDomain.
-    //   - What would happen if you used a PossibleDataFlowDomain instead? Think about it (or try it), but remember to deliver the Definite version.
-    // - Keep it simple: track only integer values. Any non-integer values should be ignored.
-    // - To test your implementation, you can use the inputs/cprop.imp file or define your own test cases.
-    // - Refer to the Java test methods discussed in class and adjust them accordingly to work with your domain.
-    // - How should integer constant values be propagated?
-    //   - Consider the following code snippet:
-    //       1. x = 1
-    //       2. y = x + 2
-    //     The expected output should be:
-    //       1. [x,1]
-    //       2. [x,1] [y,3]
-    //   - How can you retrieve the constant value of `x` to use at program point 2?
-    //   - When working with an object of type `Constant`, you can obtain its value by calling the `getValue()` method.
+    public final Identifier id;
+    public final Constant constant;
 
-//	@Override
-//	public StructuredRepresentation representation() {
-//		return new ListRepresentation(
-//				new StringRepresentation(id),
-//				new StringRepresentation(constant));
-//	}
-//
-//	@Override
-//	public CProp pushScope(
-//			ScopeToken scope)
-//			throws SemanticException {
-//		return this;
-//	}
-//
-//	@Override
-//	public CProp popScope(
-//			ScopeToken scope)
-//			throws SemanticException {
-//		return this;
-//	}
+    public CProp(Identifier id, Constant constant) {
+        this.id = id;
+        this.constant = constant;
+    }
+
+    public CProp() {
+        this(null, null);
+    }
+
+    @Override
+    public Collection<Identifier> getInvolvedIdentifiers() {
+        // This domain only deals with altering a data structure in which only one identifier is saved, i.e. the
+        // variable that can potentially contain a constant value.
+        return Collections.singleton(id);
+    }
+
+    @Override
+    public Collection<CProp> gen(
+            Identifier identifier,
+            ValueExpression valueExpression,
+            ProgramPoint programPoint,
+            DefiniteDataflowDomain<CProp> domain) throws SemanticException {
+        // Depending on the given expression, the evaluation of the constant can differ significantly. Instead of
+        // calculating everything in one method, a specific class will be instantiated based on the given expression to
+        // handle the specific scenario in isolation. This will also allow to mentally divide each task into an
+        // appropriate scope. To calculate the constant, only the identifier and the domain are passed to the evaluator.
+        return ConstantPropagationEvaluator
+                .defaultRegistry()
+                .evaluatorFor(valueExpression, EmptyHandler.INSTANCE)
+                .evaluate(new ConstantPropagationData(identifier, domain))
+                .map(Collections::singleton)
+                .orElse(Collections.emptySet());
+    }
+
+    @Override
+    public Collection<CProp> gen(
+            ValueExpression valueExpression,
+            ProgramPoint programPoint,
+            DefiniteDataflowDomain<CProp> cPropPossibleDataflowDomain) throws SemanticException {
+        // When no assignment is involved, there is no need to add any dataflow element.
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Collection<CProp> kill(Identifier identifier, ValueExpression valueExpression, ProgramPoint programPoint, DefiniteDataflowDomain<CProp> domain) throws SemanticException {
+        // In case of an assignment, this method will remove every dataflow element of the domain that contains an
+        // identifier that is equal to the given one.
+        return domain
+                .getDataflowElements()
+                .stream()
+                .filter(elem -> elem.id.equals(identifier))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Collection<CProp> kill(
+            ValueExpression valueExpression,
+            ProgramPoint programPoint,
+            DefiniteDataflowDomain<CProp> domain) throws SemanticException {
+        // When no assignment is involved, there is no need to remove any dataflow element.
+        return Collections.emptyList();
+    }
+
+	@Override
+	public StructuredRepresentation representation() {
+		return new ListRepresentation(
+				new StringRepresentation(id),
+				new StringRepresentation(constant));
+	}
+
+	@Override
+	public CProp pushScope(
+			ScopeToken scope)
+			throws SemanticException {
+		return this;
+	}
+
+	@Override
+	public CProp popScope(
+			ScopeToken scope)
+			throws SemanticException {
+		return this;
+	}
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CProp cProp = (CProp) o;
+        return Objects.equals(id, cProp.id) && Objects.equals(constant, cProp.constant);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(id);
+        result = 31 * result + Objects.hashCode(constant);
+        return result;
+    }
 }

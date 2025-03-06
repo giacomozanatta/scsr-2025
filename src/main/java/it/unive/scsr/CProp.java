f2e254c371f53b273c3538425ce14722db0df5f5
gap <gapilo.14@outlook.com>
2025-03-06 12:53:23 +0100

    CProp implementation

--- src/main/java/it/unive/scsr/CProp.java
+++ src/main/java/it/unive/scsr/CProp.java
@@ -1,49 +1,172 @@
 package it.unive.scsr;
 
-public class CProp {
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import it.unive.lisa.analysis.ScopeToken;
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.dataflow.DataflowElement;
+import it.unive.lisa.analysis.dataflow.DefiniteDataflowDomain;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.BinaryExpression;
+import it.unive.lisa.symbolic.value.UnaryExpression;
+import it.unive.lisa.symbolic.SymbolicExpression;
+import it.unive.lisa.symbolic.value.Constant;
+import it.unive.lisa.symbolic.value.Identifier;
+import it.unive.lisa.symbolic.value.Operator;
+import it.unive.lisa.symbolic.value.ValueExpression;
+import it.unive.lisa.symbolic.value.operator.AdditionOperator;
+import it.unive.lisa.symbolic.value.operator.DivisionOperator;
+import it.unive.lisa.symbolic.value.operator.ModuloOperator;
+import it.unive.lisa.symbolic.value.operator.MultiplicationOperator;
+import it.unive.lisa.symbolic.value.operator.SubtractionOperator;
+import it.unive.lisa.symbolic.value.operator.unary.NumericNegation;
+import it.unive.lisa.util.representation.ListRepresentation;
+import it.unive.lisa.util.representation.StringRepresentation;
+import it.unive.lisa.util.representation.StructuredRepresentation;
 
-    // IMPLEMENTATION NOTE:
-    // the code below is outside of the scope of the course. You can uncomment
-    // it to get your code to compile. Be aware that the code is written
-    // expecting that a field named "id" and a field named "constant" exist
-    // in this class: if you name them differently, change also the code below
-    // to make it work by just using the name of your choice instead of
-    // "id"/"constant". If you don't have these fields in your
-    // solution, then you should make sure that what you are doing is correct :)
+public class CProp implements DataflowElement<DefiniteDataflowDomain<CProp>, CProp> {
+    private final Identifier identifier;
+    private final Integer value;
 
-    // - Implement your solution using the DefiniteDataFlowDomain.
-    //   - What would happen if you used a PossibleDataFlowDomain instead? Think about it (or try it), but remember to deliver the Definite version.
-    // - Keep it simple: track only integer values. Any non-integer values should be ignored.
-    // - To test your implementation, you can use the inputs/cprop.imp file or define your own test cases.
-    // - Refer to the Java test methods discussed in class and adjust them accordingly to work with your domain.
-    // - How should integer constant values be propagated?
-    //   - Consider the following code snippet:
-    //       1. x = 1
-    //       2. y = x + 2
-    //     The expected output should be:
-    //       1. [x,1]
-    //       2. [x,1] [y,3]
-    //   - How can you retrieve the constant value of `x` to use at program point 2?
-    //   - When working with an object of type `Constant`, you can obtain its value by calling the `getValue()` method.
+    public CProp(Identifier identifier, Integer value) {
+        this.identifier = identifier;
+        this.value = value;
+    }
 
-//	@Override
-//	public StructuredRepresentation representation() {
-//		return new ListRepresentation(
-//				new StringRepresentation(id),
-//				new StringRepresentation(constant));
-//	}
-//
-//	@Override
-//	public CProp pushScope(
-//			ScopeToken scope)
-//			throws SemanticException {
-//		return this;
-//	}
-//
-//	@Override
-//	public CProp popScope(
-//			ScopeToken scope)
-//			throws SemanticException {
-//		return this;
-//	}
+    public CProp() {
+        this.identifier = null;
+        this.value = null;
+    }
+
+    @Override
+    public Collection<Identifier> getInvolvedIdentifiers() {
+        Set<Identifier> identifiers = new HashSet<>();
+        if (identifier != null) {
+            identifiers.add(identifier);
+        }
+        return identifiers;
+    }
+
+    private static Integer computeExpression(SymbolicExpression expr, DefiniteDataflowDomain<CProp> domaincontext) {
+        Integer result = null;
+
+        if (expr instanceof Constant) {
+            Constant constant = (Constant) expr;
+            if (constant.getValue() instanceof Integer) {
+                result = (Integer) constant.getValue();
+            }
+        } else if (expr instanceof Identifier) {
+            for (CProp element : domaincontext.getDataflowElements()) {
+                if (element.identifier.equals(expr)) {
+                    result = element.value;
+                    break;
+                }
+            }
+        } else if (expr instanceof UnaryExpression) {
+            UnaryExpression unaryExpression = (UnaryExpression) expr;
+            Integer evaluated = computeExpression(unaryExpression.getExpression(), domaincontext);
+            if (evaluated != null && unaryExpression.getOperator() instanceof NumericNegation) {
+                result = -evaluated;
+            } else {
+                result = evaluated;
+            }
+        } else if (expr instanceof BinaryExpression) {
+            BinaryExpression binaryExpression = (BinaryExpression) expr;
+            Integer left = computeExpression(binaryExpression.getLeft(), domaincontext);
+            Integer right = computeExpression(binaryExpression.getRight(), domaincontext);
+            if (left != null && right != null) {
+                Operator operator = binaryExpression.getOperator();
+                if (operator instanceof AdditionOperator) {
+                    result = left + right;
+                } else if (operator instanceof SubtractionOperator) {
+                    result = left - right;
+                } else if (operator instanceof MultiplicationOperator) {
+                    result = left * right;
+                } else if (operator instanceof DivisionOperator && right != 0) {
+                    result = left / right;
+                } else if (operator instanceof ModuloOperator && right != 0) {
+                    result = left % right;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public Collection<CProp> gen(Identifier id, ValueExpression expr, ProgramPoint pp, DefiniteDataflowDomain<CProp> domain)
+            throws SemanticException {
+                Set<CProp> generatedElements = new HashSet<>();
+                Integer result = computeExpression(expr, domain);
+                if (result != null) {
+                    generatedElements.add(new CProp(id, result));
+                }
+            return generatedElements;
+    }
+
+    @Override
+    public Collection<CProp> gen(ValueExpression expr, ProgramPoint pp, DefiniteDataflowDomain<CProp> domain)
+            throws SemanticException {
+                return new HashSet<>();
+    }
+
+    @Override
+    public Collection<CProp> kill(Identifier id, ValueExpression expr, ProgramPoint pp, DefiniteDataflowDomain<CProp> domain)
+            throws SemanticException {
+                Set<CProp> toRemove = new HashSet<>();
+                for (CProp element : domain.getDataflowElements()) {
+                    if (element.identifier.equals(id)) {
+                        toRemove.add(element);
+                    }
+                }
+                return toRemove;
+    }
+
+    @Override
+    public Collection<CProp> kill(ValueExpression expr, ProgramPoint pp, DefiniteDataflowDomain<CProp> domain)
+            throws SemanticException {
+                return new HashSet<>();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(identifier, value);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        CProp other = (CProp) obj;
+        return Objects.equals(identifier, other.identifier) && Objects.equals(value, other.value);
+    }
+
+
+// I've changed the field names, sorry
+
+    @Override
+    public StructuredRepresentation representation() {
+        return new ListRepresentation(
+                new StringRepresentation(identifier),
+                new StringRepresentation(value));
+    }
+
+    @Override
+    public CProp pushScope(ScopeToken scope)
+        throws SemanticException {
+        return this;
+    }
+
+    @Override
+    public CProp popScope(
+            ScopeToken scope)
+            throws SemanticException {
+        return this;
+    }
 }

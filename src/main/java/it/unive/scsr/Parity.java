db09eac498b36684396653de0d01945e42706de5
roveroniandrea <roveroniandrea@gmail.com>
2025-03-13 16:24:13 +0100

    init Parity

--- src/main/java/it/unive/scsr/Parity.java
+++ src/main/java/it/unive/scsr/Parity.java
@@ -1,6 +1,12 @@
 package it.unive.scsr;
 
-public class Parity {
+import it.unive.lisa.analysis.Lattice;
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.nonrelational.value.BaseNonRelationalValueDomain;
+import it.unive.lisa.util.representation.StringRepresentation;
+import it.unive.lisa.util.representation.StructuredRepresentation;
+
+public class Parity implements BaseNonRelationalValueDomain<Parity> {
 
     // IMPLEMENTATION NOTE:
     // the code below is outside of the scope of the course. You can uncomment
@@ -11,14 +17,79 @@ public class Parity {
     // choice. If you use methods instead of constants, change == with the
     // invocation of the corresponding method
 
-//	@Override
-//	public StructuredRepresentation representation() {
-//		if (this == TOP)
-//			return Lattice.topRepresentation();
-//		if (this == BOTTOM)
-//			return Lattice.bottomRepresentation();
-//		if (this == EVEN)
-//			return new StringRepresentation("EVEN");
-//		return new StringRepresentation("ODD");
-//	}
+    private static final Parity TOP = new Parity("TOP");
+    private static final Parity EVEN = new Parity("EVEN");
+    private static final Parity ODD = new Parity("ODD");
+    private static final Parity BOTTOM = new Parity("BOTTOM");
+
+    private final String parity;
+
+    public Parity() {
+        this("TOP");
+    }
+
+    public Parity(
+            String parity) {
+        this.parity = parity;
+    }
+
+    @Override
+    public StructuredRepresentation representation() {
+        if (this == TOP)
+            return Lattice.topRepresentation();
+        if (this == BOTTOM)
+            return Lattice.bottomRepresentation();
+        if (this == EVEN)
+            return new StringRepresentation("EVEN");
+        if (this == ODD)
+            return new StringRepresentation("ODD");
+
+        return null;
+    }
+
+    @Override
+    public int hashCode() {
+        return parity.hashCode();
+    }
+
+    @Override
+    public boolean equals(
+            Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Parity other = (Parity) obj;
+        if (parity != other.parity)
+            return false;
+        return true;
+    }
+
+    @Override
+    public Parity lubAux(Parity other) throws SemanticException {
+        // By documentation of this method, both "this" and "other" are neither bottom nor top
+        // So, the lup is always the top element
+        return TOP;
+    }
+
+    @Override
+    public boolean lessOrEqualAux(Parity other) throws SemanticException {
+        // By documentation of this method, both "this" and "other" are neither bottom nor top
+        // So they're surely not comparable
+        return false;
+    }
+
+    @Override
+    public Parity top() {
+        // Used to return the top element of this lattice
+        return TOP;
+    }
+
+    @Override
+    public Parity bottom() {
+        // Used to return the bottom element of this lattice
+        return BOTTOM;
+    }
 }

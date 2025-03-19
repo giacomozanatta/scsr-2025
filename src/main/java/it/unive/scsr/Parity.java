f33234380d44782710dacf39e018b236ad7dcd91
AndreaDaLio <884046@stud.unive.it>
2025-03-19 21:34:39 +0100

    Completed Task 2

--- src/main/java/it/unive/scsr/Parity.java
+++ src/main/java/it/unive/scsr/Parity.java
@@ -1,6 +1,128 @@
 package it.unive.scsr;
 
-public class Parity {
+import it.unive.lisa.analysis.Lattice;
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.SemanticOracle;
+import it.unive.lisa.analysis.nonrelational.value.BaseNonRelationalValueDomain;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.Constant;
+import it.unive.lisa.symbolic.value.operator.AdditionOperator;
+import it.unive.lisa.symbolic.value.operator.DivisionOperator;
+import it.unive.lisa.symbolic.value.operator.MultiplicationOperator;
+import it.unive.lisa.symbolic.value.operator.SubtractionOperator;
+import it.unive.lisa.symbolic.value.operator.binary.BinaryOperator;
+import it.unive.lisa.symbolic.value.operator.unary.NumericNegation;
+import it.unive.lisa.symbolic.value.operator.unary.UnaryOperator;
+import it.unive.lisa.util.representation.StringRepresentation;
+import it.unive.lisa.util.representation.StructuredRepresentation;
+
+import java.util.Objects;
+
+public class Parity implements BaseNonRelationalValueDomain<Parity> {
+    private static final Parity BOTTOM = new Parity("BOT");
+    private static final Parity TOP = new Parity("TOP");
+    private static final Parity EVEN = new Parity("EVEN");
+    private static final Parity ODD = new Parity("ODD");
+
+    private final String sign;
+
+    public Parity() {
+        this("TOP");
+    }
+
+    public Parity(String sign) {
+        this.sign = sign;
+    }
+
+    @Override
+    public StructuredRepresentation representation() {
+        if (this == TOP)
+            return Lattice.topRepresentation();
+        if (this == BOTTOM)
+            return Lattice.bottomRepresentation();
+        if (this == EVEN)
+            return new StringRepresentation("EVEN");
+        return new StringRepresentation("ODD");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Parity parity = (Parity) o;
+        return Objects.equals(sign, parity.sign);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(sign);
+    }
+
+    @Override
+    public Parity top() {
+        return TOP;
+    }
+
+    @Override
+    public Parity bottom() {
+        return BOTTOM;
+    }
+
+    @Override
+    public Parity lubAux(Parity other) throws SemanticException {
+        return TOP;
+    }
+
+    @Override
+    public boolean lessOrEqualAux(Parity other) throws SemanticException {
+        return false;
+    }
+
+    @Override
+    public Parity evalNonNullConstant(Constant constant, ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
+        if(constant.getValue() instanceof Integer value){
+            return value % 2 == 0 ? EVEN : ODD;
+        }
+        return TOP;
+    }
+
+    @Override
+    public Parity evalUnaryExpression(UnaryOperator operator, Parity arg, ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
+        if(operator instanceof NumericNegation){
+            return arg;
+        }
+        return TOP;
+    }
+
+    @Override
+    public Parity evalBinaryExpression(BinaryOperator operator, Parity left, Parity right, ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
+        //Check if left or right is top
+        if(left == TOP || right == TOP){
+            return TOP;
+        }
+
+        if (operator instanceof AdditionOperator || operator instanceof SubtractionOperator) {
+            if(left == EVEN && right == EVEN){
+                return EVEN;
+            }else if(left == ODD && right == ODD){
+                return EVEN;
+            }else{
+                return ODD;
+            }
+        } else if (operator instanceof MultiplicationOperator) {
+            if(left == EVEN && right == EVEN){
+                return EVEN;
+            }else if(left == ODD && right == ODD){
+                return ODD;
+            }else{
+                return EVEN;
+            }
+        } else if ( operator instanceof DivisionOperator) {
+            //Can't evaluate Division without having the values
+            return TOP;
+        }
+
+        return TOP;
+    }
 
     // IMPLEMENTATION NOTE:
     // the code below is outside of the scope of the course. You can uncomment
@@ -11,14 +133,4 @@ public class Parity {
     // choice. If you use methods instead of constants, change == with the
     // invocation of the corresponding method
 
-//	@Override
-//	public StructuredRepresentation representation() {
-//		if (this == TOP)
-//			return Lattice.topRepresentation();
-//		if (this == BOTTOM)
-//			return Lattice.bottomRepresentation();
-//		if (this == EVEN)
-//			return new StringRepresentation("EVEN");
-//		return new StringRepresentation("ODD");
-//	}
 }

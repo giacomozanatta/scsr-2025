cd5551a9936e22139ace52e79fd9fd9502952911
NicoleMarini <160406368+NicoleMarini@users.noreply.github.com>
2025-04-01 11:07:28 +0200

    taint final version

--- src/main/java/it/unive/scsr/Taint.java
+++ src/main/java/it/unive/scsr/Taint.java
@@ -1,54 +1,53 @@
 package it.unive.scsr;
 
-
 import it.unive.lisa.analysis.Lattice;
 import it.unive.lisa.analysis.SemanticException;
 import it.unive.lisa.analysis.taint.BaseTaint;
 import it.unive.lisa.util.representation.StringRepresentation;
 import it.unive.lisa.util.representation.StructuredRepresentation;
 
-
 public class Taint extends BaseTaint<Taint> {
 
-	/*
-	 * Lattice of Taint Domain 
-	 * 
-	 * T
-	 * |
-	 * C
-	 * |
-	 * BOTTOM
-	 * 
-	 */
-	private static final Taint TAINT = new Taint(true); // Tainted element (i.e. might be tainted)
-	private static final Taint CLEAN = new Taint(false);// Clean element (i.e. definitly clean)
-	private static final Taint BOTTOM = new Taint(null);// Bottom element (i.e. error state)
-	
-	
-	private Boolean isTainted; // encoding: true == "tainted", false == "clean", null == "bottom"
-	
+	private static final Taint TAINT = new Taint(true);   // Tainted
+	private static final Taint CLEAN = new Taint(false);  // Clean
+	private static final Taint BOTTOM = new Taint(null);  // Bottom
+
+	private final Boolean isTainted;
+
 	public Taint() {
-		this(true);
+		this(true); // default is tainted
 	}
-	
+
 	public Taint(Boolean taint) {
 		this.isTainted = taint;
-		
 	}
-	
+
 	@Override
 	public Taint lubAux(Taint other) throws SemanticException {
-		return TAINT;
+		if (this == BOTTOM)
+			return other;
+		if (other == BOTTOM)
+			return this;
+		if (this == TAINT || other == TAINT)
+			return TAINT;
+		return CLEAN;
 	}
 
 	@Override
 	public boolean lessOrEqualAux(Taint other) throws SemanticException {
+		if (this == other)
+			return true;
+		if (this == BOTTOM)
+			return true;
+		if (other == TAINT)
+			return true;
+		if (this == TAINT && other != TAINT)
+			return false;
 		return false;
 	}
 
 	@Override
 	public Taint top() {
-		
 		return TAINT;
 	}
 
@@ -59,16 +58,16 @@ public class Taint extends BaseTaint<Taint> {
 
 	@Override
 	public StructuredRepresentation representation() {
-		return this == BOTTOM ? Lattice.bottomRepresentation()
-					: this == CLEAN ? new StringRepresentation("_") : new StringRepresentation("#");
-
+		if (this == BOTTOM)
+			return Lattice.bottomRepresentation();
+		if (this == CLEAN)
+			return new StringRepresentation("_"); // clean
+		return new StringRepresentation("#"); // tainted
 	}
 
-	
 	@Override
 	public Taint wideningAux(Taint other) throws SemanticException {
-		
-		return TAINT;
+		return lubAux(other);
 	}
 
 	@Override
@@ -82,14 +81,12 @@ public class Taint extends BaseTaint<Taint> {
 	}
 
 	@Override
-	public boolean isAlwaysTainted() { // false because might be tainted and it is not definitly tainted
-		return false;
+	public boolean isAlwaysTainted() {
+		return this == TAINT;
 	}
 
 	@Override
 	public boolean isPossiblyTainted() {
-		
-		return this == TAINT;
+		return this == TAINT || this == CLEAN;
 	}
-
 }

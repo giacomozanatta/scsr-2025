e4b8bbac780022378906427265f0ee058c961413
SimoneBiondo <879899@stud.unive.it>
2025-04-07 23:14:32 +0200

    Rework of the Parity binary operations

--- src/main/java/it/unive/scsr/parity/BinaryFunctions.java
+++ src/main/java/it/unive/scsr/parity/BinaryFunctions.java
@@ -1,35 +1,45 @@
 package it.unive.scsr.parity;
 
-import it.unive.lisa.symbolic.value.operator.AdditionOperator;
-import it.unive.lisa.symbolic.value.operator.DivisionOperator;
-import it.unive.lisa.symbolic.value.operator.MultiplicationOperator;
-import it.unive.lisa.symbolic.value.operator.SubtractionOperator;
-import it.unive.lisa.symbolic.value.operator.binary.BinaryOperator;
 import it.unive.scsr.Parity;
-import java.util.Map;
+import it.unive.scsr.utils.BiFunctionDispatcher;
+
+import java.util.Optional;
 import java.util.function.BiFunction;
 
-public class BinaryFunctions {
+public class BinaryFunctions extends BiFunctionDispatcher<Parity, Parity> {
 
     public static final BinaryFunctions INSTANCE = new BinaryFunctions();
 
-    private final Map<Class<?>, BiFunction<Parity, Parity, Parity>> functionByClass;
-
-    private BinaryFunctions() {
-        functionByClass = Map.ofEntries(
-                Map.entry(AdditionOperator.class, (x, y) -> x == y ? Parity.EVEN : Parity.ODD),
-                Map.entry(SubtractionOperator.class, (x, y) -> x == y ? Parity.EVEN : Parity.ODD),
-                Map.entry(MultiplicationOperator.class, (x, y) -> x == y && x == Parity.ODD ? x : Parity.EVEN),
-                Map.entry(DivisionOperator.class, (x, y) -> Parity.TOP));
+    @Override
+    protected BiFunction<Parity, Parity, Parity> buildAdditionFunction() {
+        return (x, y) -> x == y ? Parity.EVEN : Parity.ODD;
     }
 
-    public BiFunction<Parity, Parity, Parity> findBy(BinaryOperator operator) {
-        return functionByClass
-                .keySet()
-                .stream()
-                .filter(k -> k.isAssignableFrom(operator.getClass()))
-                .findFirst()
-                .map(functionByClass::get)
-                .orElse(null);
+    @Override
+    protected BiFunction<Parity, Parity, Parity> buildSubtractionFunction() {
+        return (x, y) -> x == y ? Parity.EVEN : Parity.ODD;
+    }
+
+    @Override
+    protected BiFunction<Parity, Parity, Parity> buildMultiplicationFunction() {
+        return (x, y) -> x == y && x == Parity.ODD ? x : Parity.EVEN;
+    }
+
+    @Override
+    protected BiFunction<Parity, Parity, Parity> buildDivisionFunction() {
+        return (x, y) -> Parity.TOP;
+    }
+
+    @Override
+    protected Optional<Parity> inapplicable(Parity left, Parity right) {
+        if (left.isTop() || right.isTop()) {
+            // Whenever a top element is found, the calculation is stopped and then the bottom is returned. This must be
+            // done because the top element can also express something that is not a Parity element, so any calculation
+            // can actually be performed.
+            return Optional.of(Parity.TOP);
+        }
+
+        // No condition was found to stop the binary calculation, so the "inapplicable" object cannot be returned.
+        return Optional.empty();
     }
 }

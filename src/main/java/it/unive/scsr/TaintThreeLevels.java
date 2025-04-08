cc5264de6863113bc7d015454613c0545c9c7e89
SimoneBiondo <879899@stud.unive.it>
2025-04-08 11:45:28 +0200

    TaintThreeLevels-Task4: add first implementation of TaintThreeLevels

--- src/main/java/it/unive/scsr/TaintThreeLevels.java
+++ src/main/java/it/unive/scsr/TaintThreeLevels.java
@@ -1,6 +1,5 @@
 package it.unive.scsr;
 
-
 import it.unive.lisa.analysis.Lattice;
 import it.unive.lisa.analysis.SemanticException;
 import it.unive.lisa.analysis.SemanticOracle;
@@ -9,74 +8,78 @@ import it.unive.lisa.program.cfg.ProgramPoint;
 import it.unive.lisa.symbolic.value.operator.binary.BinaryOperator;
 import it.unive.lisa.util.representation.StringRepresentation;
 import it.unive.lisa.util.representation.StructuredRepresentation;
-
+import it.unive.scsr.taintthreelevels.BinaryFunctions;
+import java.util.Optional;
 
 public class TaintThreeLevels extends BaseTaint<TaintThreeLevels>  {
 
 	/*
-	 * Lattice of Taint Domain with three level
-	 * 
-	 * 	  TOP
-	 * 	/  	   \
-	 * TAINT	CLEAN
-	 *  \      /
-	 *   BOTTOM
-	 *   
-	 *   
-	 *   Element meanings:
-	 *   - TOP: might be tainted or clean
-	 *   - TAINT: definitly tainted
-	 *   - CLEAN: definitly clean
-	 *   - BOTTOM: error state
-	 * 
+	 * Finite lattice of Taint Domain with three level
+	 *
+	 *		TOP
+	 *		/ \
+	 * 	TAINT  CLEAN
+	 *  	\ /
+	 *     BOTTOM
+	 *
+	 * Element meanings:
+	 *	- TOP: might be tainted or clean
+	 *  - TAINT: definitely tainted
+	 *  - CLEAN: definitely clean
+	 *  - BOTTOM: error state
+	 *
 	 */
-	
+	public static final TaintThreeLevels TOP = new TaintThreeLevels();
+	public static final TaintThreeLevels TAINT = new TaintThreeLevels();
+	public static final TaintThreeLevels CLEAN = new TaintThreeLevels();
+	public static final TaintThreeLevels BOTTOM = new TaintThreeLevels();
+
 	@Override
 	public TaintThreeLevels lubAux(TaintThreeLevels other) throws SemanticException {
-		// TODO: to implement
-		return null;
+		// ...
+		return TOP;
 	}
 
 	@Override
 	public boolean lessOrEqualAux(TaintThreeLevels other) throws SemanticException {
-		// TODO: to implement
+		// ...
 		return false;
 	}
 
 	@Override
 	public TaintThreeLevels top() {
-		// TODO: to implement
-		return null;
+		// ...
+		return TOP;
 	}
 
 	@Override
 	public TaintThreeLevels bottom() {
-		// TODO: to implement
-		return null;
+		// ...
+		return BOTTOM;
 	}
 
 	@Override
 	protected TaintThreeLevels tainted() {
-		// TODO: to implement
-		return null;
+		// ...
+		return TAINT;
 	}
 
 	@Override
 	protected TaintThreeLevels clean() {
-		// TODO: to implement
-		return null;
+		// ...
+		return CLEAN;
 	}
 
 	@Override
 	public boolean isAlwaysTainted() {
-		// TODO: to implement
+		// ...
 		return false;
 	}
 
 	@Override
 	public boolean isPossiblyTainted() {
-		// TODO: to implement
-		return false;
+		// ...
+		return this == TAINT;
 	}
 	
 	public TaintThreeLevels evalBinaryExpression(
@@ -86,32 +89,28 @@ public class TaintThreeLevels extends BaseTaint<TaintThreeLevels>  {
 			ProgramPoint pp,
 			SemanticOracle oracle)
 			throws SemanticException {
-		// TODO: to implement
-		return null;
+		// ...
+		return Optional.ofNullable(BinaryFunctions.INSTANCE.findBy(operator))
+				.map(f -> f.apply(left, right))
+				.orElse(TOP);
 	}
 	
 	@Override
 	public TaintThreeLevels wideningAux(
 			TaintThreeLevels other)
 			throws SemanticException {
-		// TODO: to implement
-		return null;
+		// ... The default implementation of this method delegates to lubAux(BaseLattice), and is thus safe for finite
+		// lattices and ACC ones.
+		return super.wideningAux(other);
 	}
 
-
-	// IMPLEMENTATION NOTE:
-	// the code below is outside of the scope of the course. You can uncomment
-	// it to get your code to compile. Be aware that the code is written
-	// expecting that you have constants for identifying top, bottom, even and
-	// odd elements as we saw for the sign domain: if you name them differently,
-	// change also the code below to make it work by just using the name of your
-	// choice. If you use methods instead of constants, change == with the
-	// invocation of the corresponding method
-	
-		@Override
+	@Override
 	public StructuredRepresentation representation() {
-		// return this == BOTTOM ? Lattice.bottomRepresentation() : this == TOP ? Lattice.topRepresentation() : this == CLEAN ? new StringRepresentation("_") : new StringRepresentation("#");
-		return null;
+		// ...
+		if (this == BOTTOM) return Lattice.bottomRepresentation();
+		if (this == TOP) return Lattice.topRepresentation();
+		if (this == CLEAN) return new StringRepresentation("_");
+		return new StringRepresentation("#");
 	}
 	
 }

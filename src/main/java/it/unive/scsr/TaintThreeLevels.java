90db904a3b963a1070092f0a7ba6356350d008f6
SimoneBiondo <879899@stud.unive.it>
2025-04-08 13:13:27 +0200

    TaintThreeLevels-Task4: add comments

--- src/main/java/it/unive/scsr/TaintThreeLevels.java
+++ src/main/java/it/unive/scsr/TaintThreeLevels.java
@@ -6,6 +6,7 @@ import it.unive.lisa.analysis.SemanticOracle;
 import it.unive.lisa.analysis.taint.BaseTaint;
 import it.unive.lisa.program.cfg.ProgramPoint;
 import it.unive.lisa.symbolic.value.operator.binary.BinaryOperator;
+import it.unive.lisa.symbolic.value.operator.unary.UnaryOperator;
 import it.unive.lisa.util.representation.StringRepresentation;
 import it.unive.lisa.util.representation.StructuredRepresentation;
 import it.unive.scsr.taintthreelevels.BinaryFunctions;
@@ -36,49 +37,46 @@ public class TaintThreeLevels extends BaseTaint<TaintThreeLevels>  {
 
 	@Override
 	public TaintThreeLevels lubAux(TaintThreeLevels other) throws SemanticException {
-		// ...
+		// Since TAINT and CLEAN instances cannot be compared, this method will always return TOP.
 		return TOP;
 	}
 
 	@Override
 	public boolean lessOrEqualAux(TaintThreeLevels other) throws SemanticException {
-		// ...
+		// Since TAINT and CLEAN instances cannot be compared, this method will always return false.
 		return false;
 	}
 
 	@Override
 	public TaintThreeLevels top() {
-		// ...
 		return TOP;
 	}
 
 	@Override
 	public TaintThreeLevels bottom() {
-		// ...
 		return BOTTOM;
 	}
 
 	@Override
 	protected TaintThreeLevels tainted() {
-		// ...
 		return TAINT;
 	}
 
 	@Override
 	protected TaintThreeLevels clean() {
-		// ...
 		return CLEAN;
 	}
 
 	@Override
 	public boolean isAlwaysTainted() {
-		// ...
+		// If this instance represents the TAINT instance, then you need to make sure it is tainted.
 		return this == TAINT;
 	}
 
 	@Override
 	public boolean isPossiblyTainted() {
-		// ...
+		// When managing a TOP instance, the abstract domain cannot know for sure whether the path will be tainted or
+		// not.
 		return this == TOP;
 	}
 	
@@ -89,7 +87,8 @@ public class TaintThreeLevels extends BaseTaint<TaintThreeLevels>  {
 			ProgramPoint pp,
 			SemanticOracle oracle)
 			throws SemanticException {
-		// ...
+		// Extract the most appropriate function from the dispatcher and apply it to evaluate whether the expression
+		// will be tainted or not
 		return Optional.ofNullable(BinaryFunctions.INSTANCE.findBy(operator))
 				.map(f -> f.apply(left, right))
 				.orElse(TOP);
@@ -99,14 +98,13 @@ public class TaintThreeLevels extends BaseTaint<TaintThreeLevels>  {
 	public TaintThreeLevels wideningAux(
 			TaintThreeLevels other)
 			throws SemanticException {
-		// ... The default implementation of this method delegates to lubAux(BaseLattice), and is thus safe for finite
-		// lattices and ACC ones.
+		// Since this is a finite lattice, the default implementation applies. As documented "The default implementation
+		// of this method delegates to lubAux(BaseLattice), and is therefore safe for finite lattices and ACC."
 		return super.wideningAux(other);
 	}
 
 	@Override
 	public StructuredRepresentation representation() {
-		// ...
 		if (this == BOTTOM) return Lattice.bottomRepresentation();
 		if (this == TOP) return Lattice.topRepresentation();
 		if (this == CLEAN) return new StringRepresentation("_");

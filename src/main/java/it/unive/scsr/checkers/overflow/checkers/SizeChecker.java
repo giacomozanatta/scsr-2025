9045371b06e7632929e31282ebce608c404d5d54
SimoneBiondo <879899@stud.unive.it>
2025-04-23 09:15:03 +0200

    FinalTask: permit size checkers

--- src/main/java/it/unive/scsr/checkers/overflow/checkers/SizeChecker.java
+++ src/main/java/it/unive/scsr/checkers/overflow/checkers/SizeChecker.java
@@ -2,25 +2,29 @@ package it.unive.scsr.checkers.overflow.checkers;
 
 import it.unive.scsr.Intervals;
 import it.unive.scsr.checkers.OverflowChecker;
+
 import java.util.Arrays;
 import java.util.Optional;
 
 import static it.unive.scsr.utils.Logging.defaultLogger;
 
-public sealed abstract class SizeChecker permits UInt8 {
+public sealed abstract class SizeChecker permits Int16, Int32, Int8, UInt16, UInt32, UInt8 {
 
     /**
      * A record that represents whether a value has overflowed. If overflowed, it also specifies whether the value has
      * overflowed definitely or not.
+     *
      * @param isOverflowing <code>true</code> if the value is overflowing, <code>false</code> otherwise.
-     * @param definitely <code>true</code> if the value is definitely overflowing, <code>false</code> otherwise.
+     * @param definitely    <code>true</code> if the value is definitely overflowing, <code>false</code> otherwise.
      */
     public record OverflowingLevel(boolean isOverflowing, boolean definitely) {
 
         /**
          * @return The base record indicating that the analysis is unable to tell anything.
          */
-        public static OverflowingLevel base() { return new OverflowingLevel(false, false); }
+        public static OverflowingLevel base() {
+            return new OverflowingLevel(false, false);
+        }
     }
 
     protected final OverflowChecker.NumericalSize size;
@@ -33,11 +37,13 @@ public sealed abstract class SizeChecker permits UInt8 {
 
     /**
      * Try to find the most appropriate checker based on the numerical size.
+     *
      * @param size The numerical size used to find the most appropriate checker.
      * @return A subclass of {@link SizeChecker}.
      */
     public static Optional<SizeChecker> findBy(OverflowChecker.NumericalSize size) {
-        record ClassNamePair(Class<?> checker, String className) {}
+        record ClassNamePair(Class<?> checker, String className) {
+        }
 
         // Assuming that there is a direct mapping between the name representing the numeric size and the class name of
         // the subclasses of this sealed class. Because it is a sealed class, it is known at compile time what the

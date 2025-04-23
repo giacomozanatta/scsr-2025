b83a2141ad75b1cfe6cb810395ea1a4c7c9dc7a8
SimoneBiondo <879899@stud.unive.it>
2025-04-23 11:05:33 +0200

    FinalTask: add first implementation of SizeChecker isOverflowing method

--- src/main/java/it/unive/scsr/checkers/overflow/checkers/SizeChecker.java
+++ src/main/java/it/unive/scsr/checkers/overflow/checkers/SizeChecker.java
@@ -1,10 +1,13 @@
 package it.unive.scsr.checkers.overflow.checkers;
 
+import it.unive.lisa.util.numeric.MathNumber;
 import it.unive.scsr.Intervals;
 import it.unive.scsr.checkers.OverflowChecker;
 
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import static it.unive.scsr.utils.Logging.defaultLogger;
 
@@ -33,7 +36,68 @@ public sealed abstract class SizeChecker permits Int16, Int32, Int8, UInt16, UIn
         this.size = size;
     }
 
-    public abstract OverflowingLevel isOverflowing(Intervals intervals);
+    /**
+     * Determines whether the value can cause an overflow or not.
+     *
+     * @param intervals The range that represents the lower and upper numeric limit for a program element.
+     * @return The {@link OverflowingLevel} indicating whether the value could overflow and whether it will definitely
+     * happen or not.
+     */
+    public OverflowingLevel isOverflowing(Intervals intervals) {
+
+        Supplier<OverflowingLevel> possibleOverflow = () -> new OverflowingLevel(true, false);
+        Supplier<OverflowingLevel> definiteOverflow = () -> new OverflowingLevel(true, true);
+
+        // Converts a Number to its double value for use in over-approximation scenarios. Useful when precision is not
+        // critical and a uniform numeric type (double) is desired.
+        Function<Number, Double> overApproximate = Number::doubleValue;
+
+        if (intervals.isBottom()) {
+            // TODO: handle bottom in a better way.
+            return OverflowingLevel.base();
+        }
+
+        if (intervals.isTop()) {
+            // The value may exceed, but not definitively.
+            return possibleOverflow.get();
+        }
+
+        var interval = intervals.interval;
+        var min = interval.getLow();
+        var max = interval.getHigh();
+
+        if (min.isInfinite() || max.isInfinite()) {
+            // Since it is not top, it is possible to know the "overflow direction". However, this requires additional
+            // data to track.
+            possibleOverflow.get();
+        }
+
+        // From this point on the interval is finite, that is, the lower and upper bounds are represented by finite
+        // numbers.
+        var minApproximation = overApproximate.apply(minLimit());
+        var maxApproximation = overApproximate.apply(maxLimit());
+        var isUnderflow = min.lt(new MathNumber(minApproximation));
+        var isOverflow = max.gt(new MathNumber(maxApproximation));
+
+        if (isUnderflow || isOverflow) {
+            // Whenever the value is still outside the limit, the overflow is definite.
+            var alwaysDown = max.lt(new MathNumber(minLimit().longValue()));
+            var alwaysUp = min.gt(new MathNumber(maxLimit().longValue()));
+
+            // If the direction is consistently down or up, return the definite overflow value. Otherwise, return the
+            // possible overflow value, accounting for uncertainty.
+            if (alwaysDown || alwaysUp) return definiteOverflow.get();
+            return possibleOverflow.get();
+        }
+
+        // If no further information can be deduced from the specified ranges, the verifier will not assume anything and
+        // will not report any errors.
+        return OverflowingLevel.base();
+    }
+
+    public abstract Number minLimit();
+
+    public abstract Number maxLimit();
 
     /**
      * Try to find the most appropriate checker based on the numerical size.

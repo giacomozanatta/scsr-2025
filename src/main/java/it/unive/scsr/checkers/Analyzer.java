f702079d77045032eed2faad9c9c914d391e0c6b
SimoneBiondo <879899@stud.unive.it>
2025-04-24 12:56:23 +0200

    FinalTask: add getDynamicTypes to analyzer

--- src/main/java/it/unive/scsr/checkers/Analyzer.java
+++ src/main/java/it/unive/scsr/checkers/Analyzer.java
@@ -7,6 +7,14 @@ import it.unive.lisa.analysis.heap.pointbased.PointBasedHeap;
 import it.unive.lisa.analysis.nonrelational.value.TypeEnvironment;
 import it.unive.lisa.analysis.types.InferredTypes;
 import it.unive.lisa.analysis.value.ValueDomain;
+import it.unive.lisa.program.cfg.statement.VariableRef;
+import it.unive.lisa.symbolic.value.Variable;
+import it.unive.lisa.type.Type;
+import it.unive.lisa.type.Untyped;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Analyzer<V extends ValueDomain<V>> {
 
@@ -18,8 +26,9 @@ public class Analyzer<V extends ValueDomain<V>> {
 
     /**
      * Compute the exit state or throw a {@link RuntimeException}.
-     * @throws RuntimeException If the lub operator fails.
+     *
      * @return The {@link ValueDomain} contained in the exit state.
+     * @throws RuntimeException If the lub operator fails.
      */
     public V exitStateOrThrow() {
         try {
@@ -29,6 +38,29 @@ public class Analyzer<V extends ValueDomain<V>> {
                     .getValueState();
         } catch (SemanticException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * @param id        An identifier of a real program variable.
+     * @param reference A reference to a variable of the current CFG, identified by its name.
+     * @return The set of possible types that the given variable can "hold". If the types cannot be inferred, an empty
+     * set is returned.
+     */
+    public Set<Type> getDynamicTypes(Variable id, VariableRef reference) {
+        try {
+            var state = analyzer.getAnalysisStateAfter(reference).getState();
+            var dynamicType = state.getDynamicTypeOf(id, reference, state);
+
+            if (!dynamicType.isUntyped()) return Set.of(dynamicType);
+
+            return state
+                    .getRuntimeTypesOf(id, reference, state)
+                    .stream()
+                    .filter(t -> t != Untyped.INSTANCE)
+                    .collect(Collectors.toSet());
+        } catch (Exception e) {
+            return Collections.emptySet();
         }
     }
 }

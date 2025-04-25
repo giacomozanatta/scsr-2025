a2391efe6f85134a695d536f3c430dec669d667c
SimoneBiondo <879899@stud.unive.it>
2025-04-25 14:03:49 +0200

    FinalTask: update NumericInterval

--- src/main/java/it/unive/scsr/intervals/NumericInterval.java
+++ src/main/java/it/unive/scsr/intervals/NumericInterval.java
@@ -1,8 +1,10 @@
 package it.unive.scsr.intervals;
 
 import it.unive.lisa.util.numeric.MathNumber;
+import it.unive.scsr.utils.Sets;
 
-import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.Set;
 
 public class NumericInterval implements Comparable<NumericInterval> {
 
@@ -19,17 +21,6 @@ public class NumericInterval implements Comparable<NumericInterval> {
         default MathNumber toMathNumber() {
             if (this instanceof Long allowed) return new MathNumber(allowed.value);
             return new MathNumber((java.lang.Double) this.number());
-        }
-
-        /**
-         * Converts this {@link IntervalNumber} to a {@link BigDecimal}. If the {@link IntervalNumber} is a {@link Long}
-         * or {@link Double}, a new {@link MathNumber} is created directly from its primitive value.
-         *
-         * @return A new {@link MathNumber} representing the value of this interval.
-         */
-        default BigDecimal toBigDecimal() {
-            if (this instanceof Long allowed) new BigDecimal(allowed.value);
-            return BigDecimal.valueOf((java.lang.Double) this.number());
         }
 
         /**
@@ -59,7 +50,32 @@ public class NumericInterval implements Comparable<NumericInterval> {
                 return value;
             }
         }
+
+        /**
+         * Attempts to build an {@link IntervalNumber} from a given {@link Number}. This method checks the runtime type
+         * of the input {@link Number} against a set of allowed types. If the type is not allowed, an empty
+         * {@link Optional} is returned. If the type is within a set of continuous number types (e.g., Float, Double),
+         * an {@link IntervalNumber} wrapping a {@link Double} representation of the input is returned within an
+         * {@link Optional}. Otherwise (assuming the type is within the allowed set and not continuous, implying an
+         * integral type), an {@link IntervalNumber} wrapping a {@link Long} representation is returned within an
+         * {@link Optional}.
+         *
+         * @param number The {@link Number} to attempt to build an {@link IntervalNumber} from.
+         * @return An {@link  Optional} containing the built {@link IntervalNumber} if the input {@link Number}'s type
+         * is allowed; otherwise, an empty {@link Optional}.
+         */
+        static Optional<IntervalNumber> build(Number number) {
+            if (!ALLOWED_TYPES.contains(number.getClass())) return Optional.empty();
+            if (CONTINUOUS_TYPES.contains(number.getClass())) return Optional.of(new Double(number.doubleValue()));
+            return Optional.of(new Long(number.longValue()));
+        }
     }
+
+    public static final Set<Class<? extends Number>> CONTINUOUS_TYPES = Set.of(Double.class, Float.class);
+    public static final Set<Class<? extends Number>> DISCRETE_TYPES =
+            Set.of(Byte.class, Short.class, Integer.class, Long.class);
+
+    public static final Set<Class<? extends Number>> ALLOWED_TYPES = Sets.from(CONTINUOUS_TYPES, DISCRETE_TYPES);
 
     public static final NumericInterval ZERO = new NumericInterval(MathNumber.ZERO, MathNumber.ZERO);
     public static final NumericInterval INFINITY =
@@ -83,6 +99,10 @@ public class NumericInterval implements Comparable<NumericInterval> {
 
     public NumericInterval(IntervalNumber low, IntervalNumber high) {
         this(low.toMathNumber(), high.toMathNumber());
+    }
+
+    public NumericInterval(IntervalNumber singletonNumber) {
+        this(singletonNumber.toMathNumber(), singletonNumber.toMathNumber());
     }
 
     public boolean lowIsMinusInfinity() {

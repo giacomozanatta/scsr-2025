8e5f663c331781a6c96550fc4029100f947a2a1b
retrolimber2024 <908093@stud.unive.it>
2025-04-29 15:16:29 +0200

    Fixes to overflow checker

--- src/main/java/it/unive/scsr/checkers/OverflowChecker.java
+++ src/main/java/it/unive/scsr/checkers/OverflowChecker.java
@@ -26,9 +26,9 @@ import it.unive.lisa.util.numeric.MathNumber;
 import it.unive.scsr.Intervals;
 
 public class OverflowChecker implements
-SemanticCheck<
-		SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>, TypeEnvironment<InferredTypes>>> {
-	
+		SemanticCheck<
+				SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>, TypeEnvironment<InferredTypes>>> {
+
 	public enum NumericalSize {
 		INT8,  // signed integer 8-bit
 		INT16, // signed integer 16-bit
@@ -40,9 +40,9 @@ SemanticCheck<
 		FLOAT16, // signed float 16-bit
 		FLOAT32, // signed float 32-bit
 	}
-	
+
 	private NumericalSize size;
-	
+
 	public OverflowChecker(NumericalSize size) {
 		this.size = size;
 	}
@@ -52,20 +52,20 @@ SemanticCheck<
 			CheckToolWithAnalysisResults<SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>, TypeEnvironment<InferredTypes>>> tool,
 			CFG graph, Statement node) {
 
-		 // Method to analyze only the CFGs that correspond to the numeric size bound to this instance.
+		// Method to analyze only the CFGs that correspond to the numeric size bound to this instance.
 
 		if (!isCfgRelevant(graph))
 			return true; // skip this node – keep visiting to allow other checks
-		
+
 		if (node instanceof Assignment) {
 			Assignment assignment = (Assignment) node;
 			Expression leftExpression = assignment.getLeft();
-			
+
 			// Checking if each variable reference is over/under-flowing
 			if (leftExpression instanceof VariableRef) {
 				checkVariableRef(tool, (VariableRef) leftExpression, graph, node);
 			}
-			
+
 		} else {
 
 			// Checking if each variable reference is over/under-flowing
@@ -73,11 +73,11 @@ SemanticCheck<
 				checkVariableRef(tool, (VariableRef) node, graph, node);
 			}
 		}
-		
+
 		return true;
-		
+
 	}
-	
+
 	private void checkVariableRef(CheckToolWithAnalysisResults<SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>, TypeEnvironment<InferredTypes>>> tool, VariableRef varRef, CFG graph, Statement node ) {
 		Variable id = new Variable(((VariableRef) varRef).getStaticType(), ((VariableRef) varRef).getName(), ((VariableRef) varRef).getLocation());
 // TYPE CHECKER
@@ -97,9 +97,9 @@ SemanticCheck<
 		double max = getMaxValue();
 
 		for (AnalyzedCFG<SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>,
-							TypeEnvironment<InferredTypes>>> result : tool.getResultOf(graph)) {
-				SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>, TypeEnvironment<InferredTypes>> state = result.getAnalysisStateAfter(node).getState();
-				Intervals intervalAbstractValue = state.getValueState().getState(id);
+				TypeEnvironment<InferredTypes>>> result : tool.getResultOf(graph)) {
+			SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>, TypeEnvironment<InferredTypes>> state = result.getAnalysisStateAfter(node).getState();
+			Intervals intervalAbstractValue = state.getValueState().getState(id);
 
 // OVERFLOW & UNDERFLOW LOGIC
 			if (intervalAbstractValue == null || intervalAbstractValue.isBottom())
@@ -145,35 +145,35 @@ SemanticCheck<
 	private Set<Type> getPossibleDynamicTypes(
 			CheckToolWithAnalysisResults<SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>, TypeEnvironment<InferredTypes>>> tool,
 			CFG graph, Statement node, Variable id, VariableRef varRef) {
-		
-			Set<Type> possibleDynamicTypes = new HashSet<>();
-			for (AnalyzedCFG<
-					SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>,
-							TypeEnvironment<InferredTypes>>> result : tool.getResultOf(graph)) {
-				SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>, TypeEnvironment<InferredTypes>> state = result.getAnalysisStateAfter(varRef).getState();
-				try {
-					Type dynamicTypes = state.getDynamicTypeOf(id, varRef, state);
-					if(dynamicTypes != null && !dynamicTypes.isUntyped()) {
-						possibleDynamicTypes.add(dynamicTypes);
-					} else if(dynamicTypes.isUntyped()){
-						Set<Type> runtimeTypes = state.getRuntimeTypesOf(id, varRef, state);
-						if(runtimeTypes.stream().anyMatch(t -> t != Untyped.INSTANCE))
-							for( Type t : runtimeTypes)
-								possibleDynamicTypes.add(t);
-					}
-				} catch (SemanticException e) {
-					System.err.println("Cannot check " + node);
-					e.printStackTrace(System.err);
+
+		Set<Type> possibleDynamicTypes = new HashSet<>();
+		for (AnalyzedCFG<
+				SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>,
+						TypeEnvironment<InferredTypes>>> result : tool.getResultOf(graph)) {
+			SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>, TypeEnvironment<InferredTypes>> state = result.getAnalysisStateAfter(varRef).getState();
+			try {
+				Type dynamicTypes = state.getDynamicTypeOf(id, varRef, state);
+				if(dynamicTypes != null && !dynamicTypes.isUntyped()) {
+					possibleDynamicTypes.add(dynamicTypes);
+				} else if(dynamicTypes.isUntyped()){
+					Set<Type> runtimeTypes = state.getRuntimeTypesOf(id, varRef, state);
+					if(runtimeTypes.stream().anyMatch(t -> t != Untyped.INSTANCE))
+						for( Type t : runtimeTypes)
+							possibleDynamicTypes.add(t);
 				}
-	
-			}	
+			} catch (SemanticException e) {
+				System.err.println("Cannot check " + node);
+				e.printStackTrace(System.err);
+			}
+
+		}
 		return possibleDynamicTypes;
 	}
 
 
-    private boolean isNumeric(Type t) {
-        return isIntegerType(t) || isFloatingPointType(t);
-    }
+	private boolean isNumeric(Type t) {
+		return isIntegerType(t) || isFloatingPointType(t);
+	}
 
 	private boolean isIntegerType(Type t) {
 		String name = t.toString().toLowerCase();
@@ -193,7 +193,7 @@ SemanticCheck<
 			case UINT8:  return 0;
 			case UINT16: return 0;
 			case UINT32: return 0;
-			case FLOAT8: return -1.0;
+			case FLOAT8: return -240.0;
 			case FLOAT16: return -65504.0;
 			case FLOAT32: return -Float.MAX_VALUE;
 			default:      return Double.NEGATIVE_INFINITY;
@@ -208,7 +208,7 @@ SemanticCheck<
 			case UINT8:  return 255;
 			case UINT16: return 65535;
 			case UINT32: return 4294967295L;
-			case FLOAT8: return 1.0;
+			case FLOAT8: return 240.0;
 			case FLOAT16: return 65504.0;
 			case FLOAT32: return Float.MAX_VALUE;
 			default:      return Double.POSITIVE_INFINITY;
@@ -219,4 +219,3 @@ SemanticCheck<
 		return name.endsWith(size.name());
 	}
 }
-	

c542cfe25af5291736dbd7e6fa580f158b770c6c
SimoneBiondo <879899@stud.unive.it>
2025-05-02 13:36:43 +0200

    FinalTask: avoid polluting with html graphs for each test

--- src/test/java/it/unive/scsr/resources/ProgramResource.java
+++ src/test/java/it/unive/scsr/resources/ProgramResource.java
@@ -42,7 +42,6 @@ public class ProgramResource<S extends SemanticCheck<?>, V extends ValueDomain<V
             final LiSAConfiguration configuration = new DefaultConfiguration();
 
             configuration.workdir = outputDirectory;
-            configuration.analysisGraphs = LiSAConfiguration.GraphType.HTML;
             configuration.jsonOutput = true;
 
             configuration.abstractState =

0476f978d21654f2bae26202bf11632c5d59ba02
SimoneBiondo <879899@stud.unive.it>
2025-05-03 18:34:22 +0200

    FinalTask: minor refactor

--- src/main/java/it/unive/scsr/checkers/DivisionByZeroChecker.java
+++ src/main/java/it/unive/scsr/checkers/DivisionByZeroChecker.java
@@ -1,11 +1,8 @@
 package it.unive.scsr.checkers;
 
-
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 
-import it.unive.lisa.analysis.AnalysisState;
 import it.unive.lisa.analysis.AnalyzedCFG;
 import it.unive.lisa.analysis.SemanticException;
 import it.unive.lisa.analysis.SimpleAbstractState;
@@ -26,88 +23,80 @@ import it.unive.scsr.Intervals;
 import it.unive.scsr.checkers.OverflowChecker.NumericalSize;
 
 public class DivisionByZeroChecker implements
-SemanticCheck<
-		SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>, TypeEnvironment<InferredTypes>>> {
-	
-	
-	private NumericalSize size;
-	
-	public DivisionByZeroChecker(NumericalSize size) {
-		this.size = size;
-	}
+        SemanticCheck<
+                SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>, TypeEnvironment<InferredTypes>>> {
 
-	@Override
-	public boolean visit(
-			CheckToolWithAnalysisResults<SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>, TypeEnvironment<InferredTypes>>> tool,
-			CFG graph, Statement node) {
-		
-		if( node instanceof Division)
-			checkDivision(tool, graph, (Division) node);
 
-		
-		return true;
-		
-	}
+    private NumericalSize size;
 
-	private void checkDivision(
-			CheckToolWithAnalysisResults<SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>, TypeEnvironment<InferredTypes>>> tool,
-			CFG graph, Division div) {
+    public DivisionByZeroChecker(NumericalSize size) {
+        this.size = size;
+    }
 
-		for (AnalyzedCFG<SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>,
-				TypeEnvironment<InferredTypes>>> result : tool.getResultOf(graph)) {
-			AnalysisState<
-			SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>,
-					TypeEnvironment<InferredTypes>>> state = result.getAnalysisStateAfter(div.getRight());
-			
-			Set<SymbolicExpression> reachableIds = new HashSet<>();
-			Iterator<SymbolicExpression> comExprIterator = state.getComputedExpressions().iterator();
-			if(comExprIterator.hasNext()) {
-				SymbolicExpression divisor = comExprIterator.next();
-					try {
-						reachableIds
-								.addAll(state.getState().reachableFrom(divisor, div, state.getState()).elements);
-						
-						for (SymbolicExpression s : reachableIds) {
-							
-							Set<Type> types = getPossibleDynamicTypes(s, div, state.getState());
-						
-			
-							// TODO: implement type checks, it is required a numerical type
-			
-							ValueEnvironment<Intervals> valueState = state.getState().getValueState();
-							
-							Intervals intervalAbstractValue = valueState.eval((ValueExpression) s, div, state.getState());
-							
-							// TODO: add checks for division by zero
-						}
-					} catch (SemanticException e) {
-						e.printStackTrace();
-					}
-	
+    @Override
+    public boolean visit(
+            CheckToolWithAnalysisResults<SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>, TypeEnvironment<InferredTypes>>> tool,
+            CFG graph, Statement node) {
+        if (node instanceof Division) checkDivision(tool, graph, (Division) node);
+        return true;
+    }
 
-			}
-		}
-		
-	}
+    private void checkDivision(
+            CheckToolWithAnalysisResults<SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>, TypeEnvironment<InferredTypes>>> tool,
+            CFG graph, Division div) {
 
-	// compute possible dynamic types / runtime types
-	private Set<Type> getPossibleDynamicTypes(SymbolicExpression s, Division div,
-			SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>, TypeEnvironment<InferredTypes>> state) throws SemanticException {
-		
-		Set<Type> possibleDynamicTypes = new HashSet<>();
-		Type dynamicTypes = state.getDynamicTypeOf(s, div, state);
-		if(dynamicTypes != null && !dynamicTypes.isUntyped()) {
-			possibleDynamicTypes.add(dynamicTypes);
-		} else if(dynamicTypes.isUntyped()){
-			Set<Type> runtimeTypes = state.getRuntimeTypesOf(s, div, state);
-			if(runtimeTypes.stream().anyMatch(t -> t != Untyped.INSTANCE))
-				for( Type t : runtimeTypes)
-					possibleDynamicTypes.add(t);
-		}
-		
-		return possibleDynamicTypes;
-	
-	}
-	
+        for (AnalyzedCFG<SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>,
+                TypeEnvironment<InferredTypes>>> result : tool.getResultOf(graph)) {
+
+            var state = result.getAnalysisStateAfter(div.getRight());
+            var comExprIterator = state.getComputedExpressions().iterator();
+
+            if (comExprIterator.hasNext()) {
+
+                var divisor = comExprIterator.next();
+
+                try {
+
+                    var reachableIds =
+                            new HashSet<>(state.getState().reachableFrom(divisor, div, state.getState()).elements);
+
+                    for (SymbolicExpression s : reachableIds) {
+
+                        Set<Type> types = getPossibleDynamicTypes(s, div, state.getState());
+
+                        // TODO: implement type checks, it is required a numerical type
+
+                        ValueEnvironment<Intervals> valueState = state.getState().getValueState();
+
+                        Intervals intervalAbstractValue = valueState.eval((ValueExpression) s, div, state.getState());
+
+                        // TODO: add checks for division by zero
+                    }
+                } catch (SemanticException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // compute possible dynamic types / runtime types
+    private Set<Type> getPossibleDynamicTypes(SymbolicExpression s, Division div,
+                                              SimpleAbstractState<PointBasedHeap, ValueEnvironment<Intervals>, TypeEnvironment<InferredTypes>> state) throws SemanticException {
+
+        Set<Type> possibleDynamicTypes = new HashSet<>();
+        Type dynamicTypes = state.getDynamicTypeOf(s, div, state);
+        if (dynamicTypes != null && !dynamicTypes.isUntyped()) {
+            possibleDynamicTypes.add(dynamicTypes);
+        } else if (dynamicTypes.isUntyped()) {
+            Set<Type> runtimeTypes = state.getRuntimeTypesOf(s, div, state);
+            if (runtimeTypes.stream().anyMatch(t -> t != Untyped.INSTANCE))
+                for (Type t : runtimeTypes)
+                    possibleDynamicTypes.add(t);
+        }
+
+        return possibleDynamicTypes;
+
+    }
+
 
 }

8030008afac89225306b1b3fd675615f8878c83e
SimoneBiondo <879899@stud.unive.it>
2025-05-10 09:59:52 +0200

    FinalTask: add comments to Analyzer

--- src/main/java/it/unive/scsr/checkers/Analyzer.java
+++ src/main/java/it/unive/scsr/checkers/Analyzer.java
@@ -1,5 +1,6 @@
 package it.unive.scsr.checkers;
 
+import it.unive.lisa.analysis.AbstractState;
 import it.unive.lisa.analysis.AnalyzedCFG;
 import it.unive.lisa.analysis.SemanticException;
 import it.unive.lisa.analysis.SemanticOracle;
@@ -28,7 +29,8 @@ public class Analyzer<V extends ValueDomain<V>> {
 
 	private final AnalyzedCFG<SimpleAbstractState<PointBasedHeap, V, TypeEnvironment<InferredTypes>>> analyzer;
 
-	public Analyzer(AnalyzedCFG<SimpleAbstractState<PointBasedHeap, V, TypeEnvironment<InferredTypes>>> analyzer) {
+	public Analyzer(
+		AnalyzedCFG<SimpleAbstractState<PointBasedHeap, V, TypeEnvironment<InferredTypes>>> analyzer) {
 		this.analyzer = analyzer;
 	}
 
@@ -46,43 +48,70 @@ public class Analyzer<V extends ValueDomain<V>> {
 		}
 	}
 
-	public SimpleAbstractState<PointBasedHeap, V, TypeEnvironment<InferredTypes>> getStateAfter(Statement statement) {
+	/**
+	 * @param statement The instruction from which the state will be calculated.
+	 * @return The {@link AbstractState} embedded into this analysis state, containing abstract
+	 * values for program variables and memory locations after the given statement.
+	 */
+	public SimpleAbstractState<PointBasedHeap, V, TypeEnvironment<InferredTypes>> getStateAfter(
+		Statement statement) {
 		return analyzer.getAnalysisStateAfter(statement).getState();
 	}
 
+	/**
+	 * @param statement The statement for which the last evaluated expression will be evaluated.
+	 * @return The last computed expression for the given <code>Statement</code>.
+	 */
 	public List<SymbolicExpression> getComputedExpression(Statement statement) {
-		var expressionsSpliterator = analyzer.getAnalysisStateAfter(statement).getComputedExpressions().spliterator();
+		var expressionsSpliterator = analyzer.getAnalysisStateAfter(statement)
+			.getComputedExpressions().spliterator();
 		return StreamSupport.stream(expressionsSpliterator, false).toList();
 	}
 
 	/**
-	 * If the static type cannot be inferred from the specified expression, its dynamic types are retrieved. If the
-	 * dynamic types cannot be inferred, the runtime types are retrieved.
+	 * If the static type cannot be inferred from the specified expression, its dynamic types are
+	 * retrieved. If the dynamic types cannot be inferred, the runtime types are retrieved.
 	 *
 	 * @param expression   The expression to type.
 	 * @param programPoint The program point where the types are required.
 	 * @param oracle       The oracle for inter-domain communication.
 	 * @return <code>Set</code> of types that have been inferred. It may be empty.
 	 */
-	public static Set<Type> inferTypes(SymbolicExpression expression, ProgramPoint programPoint, SemanticOracle oracle) {
+	public static Set<Type> inferTypes(SymbolicExpression expression, ProgramPoint programPoint,
+		SemanticOracle oracle) {
 		try {
 			// Try to deduce the static type.
 			var staticType = expression.getStaticType();
-			if (staticType != null && !staticType.isUntyped()) return Set.of(staticType);
+			if (staticType != null && !staticType.isUntyped()) {
+				return Set.of(staticType);
+			}
 
 			// Try to deduce dynamic types.
 			var dynamicType = oracle.getDynamicTypeOf(expression, programPoint, oracle);
-			if (dynamicType != null && !dynamicType.isUntyped()) return Set.of(dynamicType);
+			if (dynamicType != null && !dynamicType.isUntyped()) {
+				return Set.of(dynamicType);
+			}
 
 			// Try to infer runtime types.
-			return oracle.getRuntimeTypesOf(expression, programPoint, oracle).stream().filter(t -> t != null && t != Untyped.INSTANCE).collect(Collectors.toSet());
+			return oracle.getRuntimeTypesOf(expression, programPoint, oracle).stream()
+				.filter(t -> t != null && t != Untyped.INSTANCE).collect(Collectors.toSet());
 		} catch (SemanticException e) {
 			// If a SemanticException occurs, the empty set is returned.
 			return Collections.emptySet();
 		}
 	}
 
-	public Set<SymbolicExpression> reachableElements(SymbolicExpression expression, ProgramPoint programPoint, SemanticOracle oracle) {
+	/**
+	 * Compute all the identifiers that are reachable starting from the given expression.
+	 *
+	 * @param expression   The expression corresponding to the starting point.
+	 * @param programPoint The ProgramPoint where the computation happens.
+	 * @param oracle       The oracle for inter-domain communication.
+	 * @return The set of all reachable expressions or an empty set if something goes wrong during
+	 * the computation.
+	 */
+	public Set<SymbolicExpression> reachableElements(SymbolicExpression expression,
+		ProgramPoint programPoint, SemanticOracle oracle) {
 		try {
 			return oracle.reachableFrom(expression, programPoint, oracle).elements;
 		} catch (SemanticException e) {
@@ -91,19 +120,34 @@ public class Analyzer<V extends ValueDomain<V>> {
 	}
 
 	/**
-	 * Checks whether the set of inferred types contains at least one NumericType.
+	 * Checks whether the set of inferred types contains at least one {@link NumericType}.
 	 *
 	 * @param expression   The expression to type.
 	 * @param programPoint The program point where the types are required.
 	 * @param oracle       The oracle for inter-domain communication.
-	 * @return <code>true</code> when at least one of the inferred types is numeric, <code>false</code> otherwise.
+	 * @return <code>true</code> when at least one of the inferred types is numeric,
+	 * <code>false</code> otherwise.
 	 */
-	public static boolean anyNumericalType(SymbolicExpression expression, ProgramPoint programPoint, SemanticOracle oracle) {
+	public static boolean anyNumericalType(SymbolicExpression expression, ProgramPoint programPoint,
+		SemanticOracle oracle) {
 		// The set of inferred types must contain at least one NumericType.
-		return inferTypes(expression, programPoint, oracle).stream().map(Type::getClass).anyMatch(NumericType.class::isAssignableFrom);
+		return inferTypes(expression, programPoint, oracle).stream().map(Type::getClass)
+			.anyMatch(NumericType.class::isAssignableFrom);
 	}
 
-	public static <T extends NonRelationalValueDomain<T>> T evalOrThrow(ValueEnvironment<T> environment, ValueExpression expression, ProgramPoint programPoint, SemanticOracle oracle) {
+	/**
+	 * Calculates a non-relational value domain for the given expression.
+	 *
+	 * @param environment  The environment used to evaluate the expression.
+	 * @param expression   The expression to evaluate
+	 * @param programPoint The program point where the evaluation happens.
+	 * @param oracle       The oracle for inter-domain communication.
+	 * @param <T>          A {@link NonRelationalValueDomain}
+	 * @return The domain calculated for the given expression.
+	 */
+	public static <T extends NonRelationalValueDomain<T>> T evalOrThrow(
+		ValueEnvironment<T> environment, ValueExpression expression, ProgramPoint programPoint,
+		SemanticOracle oracle) {
 		try {
 			return environment.eval(expression, programPoint, oracle);
 		} catch (SemanticException e) {

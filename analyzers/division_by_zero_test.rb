a6946ec01a1fa764234c2bd674d3f662a9c1a10f
SimoneBiondo <879899@stud.unive.it>
2025-05-11 11:41:43 +0200

    FinalTask: fix division_by_zero_test

--- analyzers/division_by_zero_test.rb
+++ analyzers/division_by_zero_test.rb
@@ -4,8 +4,8 @@ module Analyzers
   class DivisionByZeroTest < Base
     def mangle(messages)
       messages.transform_values do |set|
-        { descriptions: set.map { _1['warning']['description'] },
-          expressions: set.map { _1['info']['expression'] } }
+        { descriptions: set.map { _1['warning']['description'] }.join(','),
+          expressions: set.map { _1['info']['expression'] }.join(',') }
       end.map do |(location, hash)|
         { warning: "#{hash[:descriptions]} at #{location} for #{hash[:expressions]}" }
       end.then { { "#{name}" => _1 } }

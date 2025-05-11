13e709914b175514cf4dfd86e0b780e0f15387fe
SimoneBiondo <879899@stud.unive.it>
2025-05-11 11:39:22 +0200

    FinalTask: implement division_by_zero_test

--- analyzers/division_by_zero_test.rb
+++ analyzers/division_by_zero_test.rb
@@ -3,7 +3,12 @@
 module Analyzers
   class DivisionByZeroTest < Base
     def mangle(messages)
-      # ...
+      messages.transform_values do |set|
+        { descriptions: set.map { _1['warning']['description'] },
+          expressions: set.map { _1['info']['expression'] } }
+      end.map do |(location, hash)|
+        { warning: "#{hash[:descriptions]} at #{location} for #{hash[:expressions]}" }
+      end.then { { "#{name}" => _1 } }
     end
   end
 end

2b14b5bf7211ac76c03134041952edd6ba55fcc3
SimoneBiondo <879899@stud.unive.it>
2025-05-14 20:54:39 +0200

    FinalTask: update size checker

--- src/main/java/it/unive/scsr/checkers/overflow/checkers/SizeChecker.java
+++ src/main/java/it/unive/scsr/checkers/overflow/checkers/SizeChecker.java
@@ -2,14 +2,13 @@ package it.unive.scsr.checkers.overflow.checkers;
 
 import it.unive.scsr.Intervals;
 import it.unive.scsr.checkers.OverflowChecker;
-import it.unive.scsr.intervals.numbers.Numeric;
 
 import java.util.Arrays;
 import java.util.Optional;
-import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 // TODO: implement float checker
-public abstract sealed class SizeChecker permits Int16, Int32, Int8, UInt16, UInt32, UInt8 {
+public abstract sealed class SizeChecker permits DecimalChecker, IntegerChecker {
 
   /**
    * A record that represents whether a value has overflowed. If overflowed, it also specifies
@@ -38,50 +37,7 @@ public abstract sealed class SizeChecker permits Int16, Int32, Int8, UInt16, UIn
    * @return The {@link OverflowingLevel} indicating whether the value could overflow and whether it
    *     will definitely happen or not.
    */
-  public OverflowingLevel isOverflowing(Intervals intervals) {
-
-    Supplier<OverflowingLevel> possibleOverflow = () -> new OverflowingLevel(true, false);
-    Supplier<OverflowingLevel> definiteOverflow = () -> new OverflowingLevel(true, true);
-
-    if (intervals.isBottom()) {
-      return OverflowingLevel.base();
-    }
-
-    if (intervals.isTop()) {
-      // The value may exceed, but not definitively.
-      return possibleOverflow.get();
-    }
-
-    var interval = intervals.interval;
-    var min = interval.low;
-    var max = interval.high;
-
-    // From this point on the interval is finite, that is, the lower and upper bounds are
-    // represented by finite numbers or by infinity.
-    var isUnderflow = min.lessThan(minLimit());
-    var isOverflow = max.greaterThan(maxLimit());
-
-    if (isUnderflow || isOverflow) {
-      // Whenever the value is still outside the limit, the overflow is definite.
-      var alwaysDown = max.lessThan(minLimit());
-      var alwaysUp = min.greaterThan(maxLimit());
-
-      // If the direction is consistently down or up, return the definite overflow value. Otherwise,
-      // return the possible
-      // overflow value, accounting for uncertainty.
-      if (alwaysDown || alwaysUp) return definiteOverflow.get();
-      return possibleOverflow.get();
-    }
-
-    // If no further information can be deduced from the specified ranges, the verifier will not
-    // assume anything and
-    // will not report any errors.
-    return OverflowingLevel.base();
-  }
-
-  public abstract Numeric<?> minLimit();
-
-  public abstract Numeric<?> maxLimit();
+  public abstract OverflowingLevel isOverflowing(Intervals intervals);
 
   /**
    * Try to find the most appropriate checker based on the numerical size.
@@ -97,7 +53,10 @@ public abstract sealed class SizeChecker permits Int16, Int32, Int8, UInt16, UIn
     // the subclasses of this sealed class. Because it is a sealed class, it is known at compile
     // time what the
     // possible subclasses are, which allows for better reflection.
-    return Arrays.stream(SizeChecker.class.getPermittedSubclasses())
+    return Stream.of(
+            IntegerChecker.class.getPermittedSubclasses(),
+            DecimalChecker.class.getPermittedSubclasses())
+        .flatMap(Arrays::stream)
         .map(klass -> new ClassNamePair(klass, klass.getSimpleName()))
         .filter(pair -> pair.className.equalsIgnoreCase(size.name()))
         .findFirst()

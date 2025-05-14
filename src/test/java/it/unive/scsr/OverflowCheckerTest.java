0e341ff79eadb6a11c04edba905c63b03b8a918f
retrolimber2024 <908093@stud.unive.it>
2025-05-14 15:19:12 +0200

    Added second student IMP overflow

--- src/test/java/it/unive/scsr/OverflowCheckerTest.java
+++ src/test/java/it/unive/scsr/OverflowCheckerTest.java
@@ -25,7 +25,7 @@ public class OverflowCheckerTest {
     public void testOverflow() throws ParsingException, AnalysisException {
 
         // Parse input program
-        Program program = IMPFrontend.processFile("inputs/studentsIMP/880036_overflow_test_v0.imp");
+        Program program = IMPFrontend.processFile("inputs/studentsIMP/879899-overflow-wip.imp");
 
         // Configure LiSA
         LiSAConfiguration conf = new DefaultConfiguration();

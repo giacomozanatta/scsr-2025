575207236c11469646e589496e782c6ce466068e
Francesco Forcellato <francesco.forcellato@gmail.com>
2025-05-27 15:49:39 +0200

    Update DivisionByZeroTest to use correct input file path and add comprehensive division test cases

--- src/test/java/it/unive/scsr/DivisionByZeroTest.java
+++ src/test/java/it/unive/scsr/DivisionByZeroTest.java
@@ -21,7 +21,7 @@ public class DivisionByZeroTest {
 	@Test
 	public void testDivisionByZero() throws ParsingException, AnalysisException {
 		// we parse the program to get the CFG representation of the code in it
-		Program program = IMPFrontend.processFile("inputs/division-by-zero.imp");
+		Program program = IMPFrontend.processFile("inputs/875290-division-by-zero.imp");
 		//Program program = IMPFrontend.processFile("inputs/SCSR_FinalTask2025_Programs/908677-benchmark-divbyzero.imp");
 
 		// we build a new configuration for the analysis
